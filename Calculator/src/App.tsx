c83d37d0ad63d50fa101a0eb780326ccf7f92ccd
Shah <shahalambdh3@gmail.com>
2023-11-04 21:36:56 +0530

    Calculator App Completed

--- Calculator/src/App.tsx
+++ Calculator/src/App.tsx
@@ -1,34 +1,39 @@
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useState } from 'react'
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
+  Switch,
 } from 'react-native';
 import CalculatorKey from './components/CalculatorKey';
-
-
+import { ThemeContext } from './context/ThemeContext';
+import { myColors } from './styles/colors';
 
 function App(): JSX.Element {
-  
-
-
+  const [theme, setTheme] = useState('light')
 
   return (
-    <SafeAreaView>
-     <CalculatorKey />
-  
-    </SafeAreaView>
+    <ThemeContext.Provider value={theme}>
+      <SafeAreaView style={theme === 'light' ? styles.container : [styles.container, {backgroundColor: 'black'}]}>
+        <Switch 
+          value={theme === 'dark'}
+          onValueChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          ios_backgroundColor="black"
+        />
+      <CalculatorKey />
+      </SafeAreaView>
+    </ThemeContext.Provider>
+
   );
 }
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    backgroundColor: myColors.light,
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
 });
 
 export default App;
