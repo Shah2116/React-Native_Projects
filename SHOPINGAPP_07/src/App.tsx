4619211902cef5552ec223433e26861978f87bdc
Shah <shahalambdh3@gmail.com>
2023-11-06 18:52:44 +0530

    setting up navigation

---
+++ SHOPINGAPP_07/src/App.tsx
@@ -0,0 +1,41 @@
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Home from './screens/Home';
+import Details from './screens/Details';
+
+
+export type RootStackParamList = {
+  Home: undefined;
+  Details:{product:Product}
+}
+
+const Stack = createNativeStackNavigator<RootStackParamList>()
+function App(): JSX.Element {
+
+  return (
+   <NavigationContainer>
+    <Stack.Navigator initialRouteName='Home'>
+      <Stack.Screen 
+      name='Home'
+      component={Home}
+      options={{
+        title: "Treanding Product"
+      }}
+      />
+      <Stack.Screen 
+      name='Details'
+      component={Details}
+      options={{
+        title: "Product Detail"
+      }}
+      />
+      
+    </Stack.Navigator>
+   </NavigationContainer>
+  );
+}
+
+export default App;
