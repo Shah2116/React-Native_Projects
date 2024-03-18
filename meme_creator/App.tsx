830691e4bf4b714805273e5c257f4de95165f81b
Shah <shahalambdh3@gmail.com>
2024-03-18 15:50:16 +0530

    Installed Navigation and added screen components

--- meme_creator/App.tsx
+++ meme_creator/App.tsx
@@ -1,12 +1,25 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import 'react-native-gesture-handler';
+import React from 'react';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import Home from './src/screens/Home';
+import About from './src/screens/About';
+import Creator from './src/screens/Creator';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
+const Drawer = createDrawerNavigator();
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
-}
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Drawer.Navigator>
+          <Drawer.Screen name="Home" component={Home} />
+          <Drawer.Screen name="About" component={About} />
+          <Drawer.Screen name="Creator" component={Creator} />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  );
+};
 
-export default App
+export default App;
