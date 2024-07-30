3632bf467f74d856613c0fadbf31e5338ef95b66
Shah <shahalambdh3@gmail.com>
2024-07-30 16:04:30 +0530

    Nested navigation added

--- RNApp/src/Naviagrion/AppNavigation.tsx
+++ RNApp/src/Naviagrion/AppNavigation.tsx
@@ -17,34 +17,34 @@ const Drawer = createDrawerNavigator();
 function DrawerNavigator() {
   return (
     <Drawer.Navigator
-      // screenOptions={{
-      //   headerStyle: { backgroundColor: '#351401' },
-      //   headerTintColor: 'white',
-      //   sceneContainerStyle: { backgroundColor: '#3f2f25' },
-      //   drawerContentStyle: { backgroundColor: '#351401' },
-      //   drawerInactiveTintColor: 'white',
-      //   drawerActiveTintColor: '#351401',
-      //   drawerActiveBackgroundColor: '#e4baa1',
-      // }}
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#3f2f25' },
+        drawerContentStyle: { backgroundColor: '#351401' },
+        drawerInactiveTintColor: 'white',
+        drawerActiveTintColor: '#351401',
+        drawerActiveBackgroundColor: '#e4baa1',
+      }}
     >
       <Drawer.Screen
         name="Categories"
         component={CategoriesScreen}
-        // options={{
-        //   title: 'All Categories',
-        //   drawerIcon: ({ color, size }) => (
-        //     <Ionicons name="list" color={color} size={size} />
-        //   ),
-        // }}
+        options={{
+          title: 'All Categories',
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="list" color={color} size={size} />
+          ),
+        }}
       />
       <Drawer.Screen
         name="Favorites"
         component={FavoritesScreen}
-        // options={{
-        //   drawerIcon: ({ color, size }) => (
-        //     <Ionicons name="star" color={color} size={size} />
-        //   ),
-        // }}
+        options={{
+          drawerIcon: ({ color, size }) => (
+            <Ionicons name="star" color={color} size={size} />
+          ),
+        }}
       />
     </Drawer.Navigator>
   );
@@ -61,7 +61,7 @@ const AppNavigation = () => {
         >
           <Stack.Screen
             name="Categories"
-            component={CategoriesScreen}
+            component={DrawerNavigator}
             options={{
               headerShown: false,
             }}
